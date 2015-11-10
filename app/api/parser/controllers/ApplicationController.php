28f8920cde2dca112364725c4b9bb8555076a5af
Quan Yang <quanyang@live.com>
2015-11-10 22:11:57 +0800

    is parsed?

--- app/api/parser/controllers/ApplicationController.php
+++ app/api/parser/controllers/ApplicationController.php
@@ -263,7 +263,7 @@ class ApplicationController extends Controller {
                 $job_application->contact = $telephone;
                 $job_application->resume_path = $resume_path;
                 $job_application->save();
-				shell_exec("/usr/bin/java -jar ../../parser.jar './resume-uploads/$resume_path' '$user->id' '$job->id' '$job_application->id'");
+				shell_exec("/usr/bin/java -jar ../../parser.jar './resume-uploads/$resume_path' '$user->id' '$job->id' '$job_application->id' >/dev/null 2>/dev/null &");
                 echo json_encode($job_application, JSON_UNESCAPED_SLASHES);
             } else {
                 throw new \Exception('Error!');
