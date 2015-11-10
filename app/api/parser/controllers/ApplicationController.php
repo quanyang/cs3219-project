57add6dc53a830bcbd1e6d72e90f00c562be2c50
Quan Yang <quanyang@live.com>
2015-11-10 18:21:47 +0800

    jar file

--- app/api/parser/controllers/ApplicationController.php
+++ app/api/parser/controllers/ApplicationController.php
@@ -263,9 +263,7 @@ class ApplicationController extends Controller {
                 $job_application->contact = $telephone;
                 $job_application->resume_path = $resume_path;
                 $job_application->save();
-                //echo json_encode($job_application, JSON_UNESCAPED_SLASHES);
-				shell_exec("java - jar parser.jar '$resume_path' '$user->id' '$job->id' '$job_id'");
-
+				passthru("/usr/bin/java -jar ../../parser.jar './resume-uploads/$resume_path' '$user->id' '$job->id' '$job_id'");
                 echo json_encode($job_application, JSON_UNESCAPED_SLASHES);
             } else {
                 throw new \Exception('Error!');
